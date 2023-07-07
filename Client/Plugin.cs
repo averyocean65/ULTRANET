6ec68a2cc4e2ca18b3a7b6892c9daba2fe7542ba
averyocean65 <averytoad65@gmail.com>
2023-07-07 18:45:55 +0200

    Basic Project

---
+++ Client/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace ULTRANET.Client
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string pluginGuid = "com.ultranet.client";
+        public const string pluginName = "ULTRANET Client";
+        public const string pluginVersion = "0.1.0";
+
+        private void Awake()
+        {
+            Logger.LogInfo("ULTRANET Initialized.");
+        }
+
+        private void OnGUI()
+        {
+            // UI
+            GUI.Label(new Rect(0, 0, 100, 100), "<b>ULTRANET</b>");
+
+            if (GUI.Button(new Rect(0, 50, 100, 50), "CONNECT"))
+            {
+                Logger.LogError("TODO: Networking");
+            }
+        }
+    }
+}
